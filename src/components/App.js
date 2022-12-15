a9c7ee8ddf05c5f2ef13f93d490c57e3d2c67f91
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2022-12-15 09:55:33 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
 import '../styles/App.css';
 
 const App = () => {
   //Write your code here
+  const [searchValue, setSearchValue] = useSearchParams();
+  let id;
+  let name="";
+  let designation="";
+  id=searchValue.get("id");
+  name=searchValue.get("name");
+  designation=searchValue.get("designation");
+  return (
+    <div className='details'>
+      Employee {id} named {name} works as {designation}
+    </div>
+  )
 }
 
 export default App;
