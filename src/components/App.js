c5a1703f358dd47e8239669d3b0a0f973511413d
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2022-12-15 16:38:26 +0530

    useMemo used

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom';
-import '../styles/App.css';
-
+import React,{ useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
+import "../styles/App.css";
 const App = () => {
   //Write your code here
-  const [searchValue, setSearchValue] = useSearchParams();
-  let id;
-  let name="";
-  let designation="";
-  id=searchValue.get("id");
-  name=searchValue.get("name");
-  designation=searchValue.get("designation");
+  const [searchValue] = useSearchParams();
+  let info = useMemo(
+    () => ({
+      id: searchValue.get("id"),
+      name: searchValue.get("name"),
+      designation: searchValue.get("designation"),
+    }),
+    []
+  );
+
   return (
-    <>
-    <button onClick={()=>setSearchValue({id:2,name:"Robin", designation:"scientist"})}>Employ details</button>
-    <div className='details'>.
-      Employee {id} named {name} works as {designation}
+    <div className="details">
+      Employee {info.id} named {info.name} works as {info.designation}
     </div>
-    </>
-  )
-}
+  );
+};
 
 export default App;
